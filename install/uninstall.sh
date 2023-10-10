8129eb55415658733ea0d8975299f6abe7bb4847
sixther-dc <382998946@qq.com>
2023-10-10 10:38:11 +0800

    调整request-log daashboard的细节

--- install/uninstall.sh
+++ install/uninstall.sh
@@ -2,8 +2,15 @@
 
 source ./helper.sh
 NAMESPACE=$(__readini global NAMESPACE)
+INFLUXDB_HOST=$(__readini influxdb INFLUXDB_HOST)
+INFLUXDB_PV_PATH=$(__readini influxdb INFLUXDB_PV_PATH)
+GRAFANA_HOST=$(__readini grafana GRAFANA_HOST)
+GRAFANA_PV_PATH=$(__readini grafana GRAFANA_PV_PATH)
 
-# kubectl delete ns $NAMESPACE
-# kubectl delete pv pv-ebpf-influxdb
+kubectl delete ns $NAMESPACE
+kubectl delete pv pv-kubebpf-influxdb
+kubectl delete pv pv-kubebpf-grafana
 
-kubectl delete deploy grafana -n dc-ebpf
+echo "kubebpf uninstalled..."
+echo "please delete directory of influxdb, node: $INFLUXDB_HOST, dir: $INFLUXDB_PV_PATH ."
+echo "please delete directory of grafana, node: $GRAFANA_HOST, dir: $GRAFANA_PV_PATH ."
